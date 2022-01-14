5811b314419d23cd22267c6d4f449249be2767b5
Joao <progjvdallagnol2001@gmail.com>
2022-01-14 10:17:23 -0300

    instalando e incluindo o style-loader e css-loader no projeto, e finzalizando a parte de gerar a senha nos geradores

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/css/classes.css'
+import './assets/css/elements.css'
+import './assets/css/menu.css'
+import './assets/css/style.css'
+
+import formGeraSenha from './modules/formGeraSenha'
+
+formGeraSenha();
